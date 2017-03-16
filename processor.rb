49ae9e3585b74189795a2b975eaa99b114aaecef
Katherine Wu <kwu@newrelic.com>
2017-03-15 20:07:31 -0700

    Process input

--- processor.rb
+++ processor.rb
@@ -17,6 +17,14 @@ class Opportunity
     self.title == other.title && self.org == other.org && self.location == other.location &&
       self.min_pay == other.min_pay && self.max_pay == other.max_pay
   end
+
+  def <(other)
+    self.title < other.title
+  end
+
+  def >(other)
+    self.title > other.title
+  end
 end
 
 class Processor
@@ -27,5 +35,22 @@ class Processor
                       location: "#{attributes[2]}, #{attributes[3]}",
                       min_pay: attributes[4].to_i, max_pay: attributes[5].to_i)
     end
+
+    def run
+      input = <<~eos
+        Lead Chef, Chipotle, Denver, CO, 10, 15
+        Stunt Double, Equity, Los Angeles, CA, 15, 25
+        Manager of Fun, IBM, Albany, NY, 30, 40
+        Associate Tattoo Artist, Tit 4 Tat, Brooklyn, NY, 250, 275
+        Assistant to the Regional Manager, IBM, Scranton, PA, 10, 15
+        Lead Guitarist, Philharmonic, Woodstock, NY, 100, 200
+      eos
+
+      opportunities = input.split("\n").map {|line| parse_line(line).to_s}
+
+      "All Opportunities\n" << opportunities.sort.join("\n")
+    end
   end
 end
+
+puts Processor.run
--- processor_test.rb
+++ processor_test.rb
@@ -20,17 +20,17 @@ class ProcessorTest < Minitest::Test
     assert_equal opportunity, Processor.parse_line(line)
   end
 
-  # def test_return_exact_expected_output
-  #   expected = <<~eos
-  #     All Opportunities
-  #     Title: Assistant to the Regional Manager, Organization: IBM, Location: Scranton, PA, Pay: 10-15
-  #     Title: Associate Tattoo Artist, Organization: Tit 4 Tat, Location: Brooklyn, NY, Pay: 250-275
-  #     Title: Lead Chef, Organization: Chipotle, Location: Denver, CO, Pay: 10-15
-  #     Title: Lead Guitarist, Organization: Philharmonic, Location: Woodstock, NY, Pay: 100-200
-  #     Title: Manager of Fun, Organization: IBM, Location: Albany, NY, Pay: 30-40
-  #     Title: Stunt Double, Organization: Equity, Location: Los Angeles, CA, Pay: 15-25
-  #   eos
+  def test_return_exact_expected_output
+    expected = <<~eos
+      All Opportunities
+      Title: Assistant to the Regional Manager, Organization: IBM, Location: Scranton, PA, Pay: 10-15
+      Title: Associate Tattoo Artist, Organization: Tit 4 Tat, Location: Brooklyn, NY, Pay: 250-275
+      Title: Lead Chef, Organization: Chipotle, Location: Denver, CO, Pay: 10-15
+      Title: Lead Guitarist, Organization: Philharmonic, Location: Woodstock, NY, Pay: 100-200
+      Title: Manager of Fun, Organization: IBM, Location: Albany, NY, Pay: 30-40
+      Title: Stunt Double, Organization: Equity, Location: Los Angeles, CA, Pay: 15-25
+    eos
 
-  #   assert_equal expected, Processor.run
-  # end
+    assert_equal expected.chomp, Processor.run
+  end
 end
